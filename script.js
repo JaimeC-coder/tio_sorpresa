44f80f67d883b33f7e35219e9e76ad8b1306b8a0
JaimeC-code <centurionjaime@gmail.com>
2023-07-30 14:58:31 -0500

    cambios finales

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-// Obtener referencia a los elementos del formulario
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+ const backgroundMusic = document.getElementById("background-music");
+backgroundMusic.volume = 0.3; 
+backgroundMusic.play();
+}, 1000);
+});
 const nameInput = document.getElementById("name");
 const numberInput = document.getElementById("number");
 const foodChoiceSelect = document.getElementById("food-choice");
 const additionalInfoTextarea = document.getElementById("additional-info");
 const backgroundMusic = document.getElementById("background-music");
-// const name = nameInput.value;
-// const number = numberInput.value;
-const volume = 0.03;
+const tableBody = document.querySelector("#table tbody");
+let rowCount = 1; 
+
+
+const volume = 0.3;
 backgroundMusic.volume = volume;
 
 function openPopup() {
@@ -25,24 +33,42 @@ function closePopup() {
 document.querySelector(".btn-rsvp").addEventListener("click", openPopup);
 
 function agregarOpcion() {
-  alert("Opción agregada"+ nameInput.value);
- 
-  // const foodChoice =
-  //   foodChoiceSelect.options[foodChoiceSelect.selectedIndex].text;
+  alert("Opción agregada"+ nameInput.value + " escogio el plato " +foodChoiceSelect.value);
+  const name = nameInput.value;
+  const foodChoice = foodChoiceSelect.options[foodChoiceSelect.selectedIndex].text;
 
-  // Construir el texto a mostrar en el textarea
-  const textToShow = `Nombre: ${name}\nCantidad de invitados: ${number}\nComida seleccionada: ${foodChoice}\n\n`;
+  const newRow = `
+  <tr>
+    <td>${rowCount}</td>
+    <td>${name}</td>
+    <td>${foodChoice}</td>
+    <td><button type="button" onclick="removerFila(this)">Remover</button></td>
+  </tr>
+`;
+tableBody.insertAdjacentHTML("beforeend", newRow);
 
-  // Agregar el texto al textarea (manteniendo el contenido previo)
-  additionalInfoTextarea.value += textToShow;
+// Incrementar el contador de filas para el índice
+rowCount++;
 
-  // Limpiar los campos del formulario después de agregar la opción al textarea
+// Limpiar los campos del formulario después de agregar la opción a la tabla
+nameInput.value = "";
+numberInput.value = "";
+foodChoiceSelect.selectedIndex = 0;
+}
+
+function removerFila(button) {
+  const row = button.parentNode.parentNode;
+  row.remove();
+}
+
+
+
+function limpiar(){
   nameInput.value = "";
   numberInput.value = "";
   foodChoiceSelect.selectedIndex = 0;
 }
-
-// Función que se ejecuta cuando el formulario se envía
+ 
 function onSubmitForm(event) {
   event.preventDefault();
 
